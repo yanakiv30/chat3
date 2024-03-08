5f2b0cf6abc05e3fcd2b0a3fea2671632ac98c2e
yanakiv30 <138647535+yanakiv30@users.noreply.github.com>
2024-03-08 22:29:19 +0200

    Context-without-components

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,24 +1,25 @@
-import { useContext } from "react";
-import { NavLink } from "react-router-dom";
+// import { useContext } from "react";
+// import { NavLink } from "react-router-dom";
 
-function UserList() {
-  const { users, loggedInUser, isUserActive } = useContext(PostContext);
-  return (
-    <div className="user-list-container">
-      <h3>Chat with:</h3>
-      <br></br>
-      <ul>
-        {users
-          .filter((user) => user.id !== loggedInUser.id)
-          .map((user) => (
-            <li key={user.id}>
-              {/* <input type="checkbox" /> */}
+// function UserList() {
+//    const { users, loggedInUser} = useContext(ChatContext);
 
-              <NavLink to={`/messages/${user.id}`}>{user.username}</NavLink>
-            </li>
-          ))}
-      </ul>
-    </div>
-  );
-}
-export default UserList;
+//   return (
+//     <div className="user-list-container">
+//       <h3>Chat with:</h3>
+//       <br></br>
+//       <ul>
+//         {users
+//           .filter((user) => user.id !== loggedInUser.id)
+//           .map((user) => (
+//             <li key={user.id}>
+//               {/* <input type="checkbox" /> */}
+
+//               <NavLink to={`/messages/${user.id}`}>{user.username}</NavLink>
+//             </li>
+//           ))}
+//       </ul>
+//     </div>
+//   );
+// }
+// export default UserList;
--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,94 +1,92 @@
-import { useContext, useState } from "react";
-import { useParams } from "react-router-dom";
-import { v4 as uuid } from "uuid";
-const API_URL = "http://localhost:3001";
+// import { useContext, useState} from "react";
+// import { useParams } from "react-router-dom";
+// import { v4 as uuid } from "uuid";
+// const API_URL = "http://localhost:3001";
 
-function UserProfile() {
-  const { setMessages, loggedInUser, messages, users } = useContext(PostContext);
-  const [newMessage, setNewMessage] = useState("");
-    const params = useParams();
-  const userInListId = params.userId;
-  const userName = users.find((x) => x.id === userInListId).username;
+// function UserProfile() {
+//   const {setMessages, loggedInUser, messages, users} = useContext(ChatContext);
 
-  function leftMessage(message) {
-    return (
-      message.receiverId === loggedInUser.id &&
-      message.senderId === userInListId
-    );
-  }
-  function rightMessage(message) {
-    return (
-      message.receiverId === userInListId &&
-      message.senderId === loggedInUser.id
-    );
-  }
+//   const [newMessage, setNewMessage] = useState("");
+//     const params = useParams();
+//   const userInListId = params.userId;
+//   const userName = users.find((x) => x.id === userInListId).username;
 
-  const largeMessages = messages.filter(
-    (message) => leftMessage(message) || rightMessage(message));
-   const  userMessages= largeMessages
-   .slice(Math.max(largeMessages.length - 10, 0)); //messages<10
-  //
-  
+//   function leftMessage(message) {
+//     return (
+//       message.receiverId === loggedInUser.id &&
+//       message.senderId === userInListId
+//     );
+//   }
+//   function rightMessage(message) {
+//     return (
+//       message.receiverId === userInListId &&
+//       message.senderId === loggedInUser.id
+//     );
+//   }
 
-  const handleSendMessage = () => {
-    if (newMessage.trim() !== "") {
-      const newMessageObject = {
-        id: uuid(),
-        senderId: loggedInUser.id,
-        receiverId: userInListId,
-        senderUsername: loggedInUser.username,
-        content: newMessage,
-      };
+//   const largeMessages = messages.filter(
+//     (message) => leftMessage(message) || rightMessage(message));
+//    const  userMessages= largeMessages
+//    .slice(Math.max(largeMessages.length - 10, 0)); //messages<10
+//   //
 
-      fetch(`${API_URL}/messages`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(newMessageObject),
-      })
-        .then((response) => response.json())
-        .then((data) => setMessages([...messages, data]))
-        .catch((error) => console.error("Error posting message:", error));
+//   const handleSendMessage = () => {
+//     if (newMessage.trim() !== "") {
+//       const newMessageObject = {
+//         id: uuid(),
+//         senderId: loggedInUser.id,
+//         receiverId: userInListId,
+//         senderUsername: loggedInUser.username,
+//         content: newMessage,
+//       };
 
-      setNewMessage("");
-    }
-  };
+//       fetch(`${API_URL}/messages`, {
+//         method: "POST",
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify(newMessageObject),
+//       })
+//         .then((response) => response.json())
+//         .then((data) => setMessages([...messages, data]))
+//         .catch((error) => console.error("Error posting message:", error));
 
-  return (
-    <div className="profile-wrapper">
-      <div className="search-bar">🔍 Search</div>
-      <div className="chat-with">
-        <h3> Chat with {userName}</h3>
-      </div>
-      <div className="user-profile-container">
-        <ul className="messages-container">
-          {userMessages.map((message) => (
-            <li
-              className={`message ${
-                leftMessage(message) ? "message-left" : "message-right"
-              }`}
-              key={message.id}
-            >
-              <strong>{message.senderUsername}:</strong> {message.content}
-            </li>
-          ))}
-        </ul>
+//       setNewMessage("");
+//     }
+//   };
 
-        <div className="message-send">
-          <input
-            type="text"
-            value={newMessage}
-            onChange={(e) => setNewMessage(e.target.value)}
-            placeholder="Type your message..."
-          />
-          <button onClick={handleSendMessage}>Send</button>
-          
-            
-          
-        </div>
-      </div>
-    </div>
-  );
-}
-export default UserProfile;
+//   return (
+//     <div className="profile-wrapper">
+//       <div className="search-bar">🔍 Search</div>
+//       <div className="chat-with">
+//         <h3> Chat with {userName}</h3>
+//       </div>
+//       <div className="user-profile-container">
+//         <ul className="messages-container">
+//           {userMessages.map((message) => (
+//             <li
+//               className={`message ${
+//                 leftMessage(message) ? "message-left" : "message-right"
+//               }`}
+//               key={message.id}
+//             >
+//               <strong>{message.senderUsername}:</strong> {message.content}
+//             </li>
+//           ))}
+//         </ul>
+
+//         <div className="message-send">
+//           <input
+//             type="text"
+//             value={newMessage}
+//             onChange={(e) => setNewMessage(e.target.value)}
+//             placeholder="Type your message..."
+//           />
+//           <button onClick={handleSendMessage}>Send</button>
+
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+// export default UserProfile;
