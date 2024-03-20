45858e7c39e8f447a2fefb2c494df3a5b5a38d4d
yanakiv30 <138647535+yanakiv30@users.noreply.github.com>
2024-03-20 05:22:07 +0200

    buttons

--- src/components/SettingsGroup.js
+++ src/components/SettingsGroup.js
@@ -3,13 +3,21 @@ import { useContext } from "react"
 export default function SettingsGroup({ChatContext}) {
     const {groups,idSettings}= useContext(ChatContext);   
     const groupToSet=groups.filter(group=> group.id===idSettings)[0].name;    
+  
+    function addUser() {};
+
+   function deleteUser() {};
+
+   function deleteGroup() {};
+   
+   
     return (
         <div className="settings" >
             <p>Settings {groupToSet}</p>           
            <div className="wrapper">
-           <button>Add User</button>
-           <button>Delete User</button>
-            <button>Delete Group</button>           
+           <button onClick={addUser}>Add User</button>
+           <button onClick={deleteUser}>Delete User</button>
+            <button onClick={deleteGroup}>Delete Group</button>           
            </div>
         </div>
     )
