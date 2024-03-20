9b9cc383a4ae8ba0c2264bcdc2f8f8954c4ae890
yanakiv30 <138647535+yanakiv30@users.noreply.github.com>
2024-03-20 06:53:25 +0200

    deleteUserInit

---
+++ src/components/DeleteUser.js
@@ -0,0 +1,7 @@
+export default function DeleteUser() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
--- src/components/SettingsGroup.js
+++ src/components/SettingsGroup.js
@@ -7,7 +7,9 @@ export default function SettingsGroup({ ChatContext }) {
 console.log(idSettings)
   function addUser() {}
 
-  function deleteUser() {}
+  function deleteUser(idSettings) {
+    console.log(groups.filter(group=>group.id===idSettings)[0].members)
+  }
 
   function deleteGroup(groupId) {
 //    console.log(groupId)
@@ -32,8 +34,7 @@ console.log(idSettings)
       <p>Settings {groupToSet}</p>
       <div className="wrapper">
         <button onClick={addUser}>Add User</button>
-        <button onClick={deleteUser}>Delete User</button>
-        
+        <button onClick={()=>deleteUser(idSettings)}>Delete User</button>        
         <button onClick={()=>deleteGroup(idSettings)}>Delete Group</button>
       </div>
     </div>
