328ae46d894e75c865660b7f545463dcf800ed55
yanakiv30 <138647535+yanakiv30@users.noreply.github.com>
2024-04-16 18:48:43 +0300

    SendUserMessage

---
+++ src/features/users/SendUserMessage.tsx
@@ -0,0 +1,19 @@
+export default function SendUserMessage({newMessage, setNewMessage,handleSendMessage}:any) {
+    return (
+        <div className="message-send">
+        <input
+          type="text"
+          value={newMessage}
+          onChange={(e) => setNewMessage(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              e.preventDefault();
+              handleSendMessage();
+            }
+          }}
+          placeholder="Type your message..."
+        />
+        <button onClick={handleSendMessage}>Send</button>
+      </div>
+    )
+}
--- src/pages/UserMessages.tsx
+++ src/pages/UserMessages.tsx
@@ -7,6 +7,7 @@ import { useState } from "react";
 import { useAppSelector } from "../store";
 import { newMessageObjectFunc } from "../utils/messageUtils";
 import UserMessagesContainer from "../features/users/UserMessagesContainer";
+import SendUserMessage from "../features/users/SendUserMessage";
 const API_URL = "http://localhost:3001";
 
 function UserMessages() {
@@ -69,21 +70,8 @@ function UserMessages() {
           searchMessage={searchMessage}
           handleDeleteMessages={handleDeleteMessages}
         />
-        <div className="message-send">
-          <input
-            type="text"
-            value={newMessage}
-            onChange={(e) => setNewMessage(e.target.value)}
-            onKeyDown={(e) => {
-              if (e.key === "Enter") {
-                e.preventDefault();
-                handleSendMessage();
-              }
-            }}
-            placeholder="Type your message..."
-          />
-          <button onClick={handleSendMessage}>Send</button>
-        </div>
+        <SendUserMessage newMessage={newMessage} setNewMessage={setNewMessage}
+         handleSendMessage={handleSendMessage}/>
       </div>
     </div>
   );
