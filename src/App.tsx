ca28a90ce14d3be9a3332bba15cfaef2dfdf2e6d
yanakiv30 <138647535+yanakiv30@users.noreply.github.com>
2024-06-09 11:21:24 +0300

    supabaseRealtime

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useCallback, useEffect } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 
@@ -11,14 +11,15 @@ import LoginOrSignUp from "./pages/LoginOrSignUp";
 import AllRoutes from "./AllRoutes";
 import { getTeams, getUsers } from "./services/apiGroups";
 import Spinner from "./utils/Spinner";
+import supabase from "./services/supabase";
 
 function App() {
   const dispatch = useDispatch();
   const { loggedInUser } = useAppSelector((store) => store.user);
-  let { isRegister, isLoading } = useAppSelector((store) => store.user);
-
-  useEffect(() => {
+  let { isLoading } = useAppSelector((store) => store.user);
+  const loadStateFromBackend = useCallback(() => {
     if (!loggedInUser) return;
+
     getUsers()
       .then((data) => dispatch(setUsers(data)))
       .catch((error) => console.error("Error fetching users:", error));
@@ -26,9 +27,87 @@ function App() {
     getTeams(+loggedInUser.id)
       .then((data) => dispatch(setTeams(data)))
       .catch((error) => console.error("Error fetching teams", error));
+  }, [dispatch, loggedInUser]);
 
-    
-  }, [dispatch, isRegister, isLoading, loggedInUser]);
+  useEffect(loadStateFromBackend, [loadStateFromBackend]);
+
+  useEffect(() => {
+    const messageSubscription = supabase
+      .channel("messages")
+      .on(
+        "postgres_changes",
+        { event: "INSERT", schema: "public", table: "messages" },
+        loadStateFromBackend
+      )
+      .on(
+        "postgres_changes",
+        { event: "UPDATE", schema: "public", table: "messages" },
+        loadStateFromBackend
+      )
+      .on(
+        "postgres_changes",
+        { event: "DELETE", schema: "public", table: "messages" },
+        loadStateFromBackend
+      )
+      .subscribe();
+
+    const userSubscription = supabase
+      .channel("users")
+      .on(
+        "postgres_changes",
+        { event: "INSERT", schema: "public", table: "users" },
+        loadStateFromBackend
+      )
+      .on(
+        "postgres_changes",
+        { event: "UPDATE", schema: "public", table: "users" },
+        loadStateFromBackend
+      )
+      .on(
+        "postgres_changes",
+        { event: "DELETE", schema: "public", table: "users" },
+        loadStateFromBackend
+      )
+      .subscribe();
+
+    const teamsSubscription = supabase
+      .channel("teams")
+      .on(
+        "postgres_changes",
+        { event: "INSERT", schema: "public", table: "teams" },
+        loadStateFromBackend
+      )
+      .on(
+        "postgres_changes",
+        { event: "UPDATE", schema: "public", table: "teams" },
+        loadStateFromBackend
+      )
+      .on(
+        "postgres_changes",
+        { event: "DELETE", schema: "public", table: "teams" },
+        loadStateFromBackend
+      )
+      .subscribe();
+
+    const teamsMembersSubscription = supabase
+      .channel("teams_members")
+      .on(
+        "postgres_changes",
+        { event: "INSERT", schema: "public", table: "teams_members" },
+        loadStateFromBackend
+      )
+      .on(
+        "postgres_changes",
+        { event: "UPDATE", schema: "public", table: "teams_members" },
+        loadStateFromBackend
+      )
+      .on(
+        "postgres_changes",
+        { event: "DELETE", schema: "public", table: "teams_members" },
+        loadStateFromBackend
+      )
+      .subscribe();
+  }, [loadStateFromBackend]);
 
   return (
     <Router>
