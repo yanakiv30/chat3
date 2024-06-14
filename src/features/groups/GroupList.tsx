6f68e138e00757446425d5bdae14345d004a5605
yanakiv30 <yanakiv30@gmail.com>
2024-06-14 09:24:09 +0300

    useRef issue

--- src/features/groups/GroupList.tsx
+++ src/features/groups/GroupList.tsx
@@ -4,6 +4,7 @@ import Avatar from "../users/Avatar";
 import { useAppSelector } from "../../store";
 import { setArrFlashIdBool } from "./groupSlice";
 import { useDispatch } from "react-redux";
+import { useEffect, useRef } from "react";
 
 export default function GroupList() {
   const dispatch=useDispatch();
@@ -19,19 +20,41 @@ export default function GroupList() {
         )
       : localTeams.filter((team) => team.name !== "");
 
-  //const arrayWithFlashTeamId: { [key: string]: boolean }[] = [];
+  // const arrayWithFlashTeamId: { [key: string]: boolean }[] = [];
 
-  searchedTeams.map((team) => {
-    const id = team.id;
-    const isNewMessage = team.id === teamWithNewMessage.team_id;
-    const obj: { [key: string]: boolean } = {};
-    obj[id] = isNewMessage;
-    isNewMessage&&dispatch(setArrFlashIdBool(obj))
-    return null;
-  });
+  // searchedTeams.map((team) => {
+  //   const id = team.id;
+  //   const isNewMessage = team.id === teamWithNewMessage.team_id;
+  //   const obj: { [key: string]: boolean } = {};
+  //   obj[id] = isNewMessage;
+  //  // isNewMessage&&dispatch(setArrFlashIdBool(obj));
+  //   isNewMessage&& arrayWithFlashTeamId.push(obj);
+  //   return null;
+  // });
  
-  //console.log("arrayWithFlashTeamId", arrayWithFlashTeamId);
-  console.log("arrFlasId", arrFlashIdBool);
+  // console.log("arrayWithFlashTeamId", arrayWithFlashTeamId);
+  // //console.log("arrFlasId", arrFlashIdBool);
+  type FlashTeamId = { [key: number]: boolean };
+  const arrayWithFlashTeamIdRef = useRef<FlashTeamId[]>([]);
+  useEffect(() => {
+    // Reset the ref value to an empty array before populating it
+    arrayWithFlashTeamIdRef.current = [];
+
+    // Iterate over searchedTeams and populate arrayWithFlashTeamIdRef
+    searchedTeams.forEach((team) => {
+      const id = team.id;
+      const isNewMessage = team.id === teamWithNewMessage.team_id;
+      const obj = { [id]: isNewMessage };
+
+      if (isNewMessage) {
+        arrayWithFlashTeamIdRef.current.push(obj);
+      }
+    });
+
+    // Log the ref value to verify the results
+    console.log(arrayWithFlashTeamIdRef.current);
+  }, [searchedTeams, teamWithNewMessage]);
+
 
   return (
     <div>
