1c6dcc253b68216942ed30fd34526efcd9703d90
yanakiv30 <138647535+yanakiv30@users.noreply.github.com>
2024-06-14 19:18:54 +0300

    flashedTeamsIds

--- src/features/groups/GroupList.tsx
+++ src/features/groups/GroupList.tsx
@@ -7,9 +7,9 @@ import { useDispatch } from "react-redux";
 import { useState } from "react";
 
 export default function GroupList() {
-  const dispatch=useDispatch();
+  const dispatch = useDispatch();
   const { loggedInUser, searchQuery } = useAppSelector((store) => store.user);
-  const { localTeams, teamWithNewMessage ,arrFlashIdBool} = useAppSelector(
+  const { localTeams, teamWithNewMessage, arrFlashIdBool } = useAppSelector(
     (store) => store.group
   );
 
@@ -20,46 +20,19 @@ export default function GroupList() {
         )
       : localTeams.filter((team) => team.name !== "");
 
-  //const arrayWithFlashTeamId: { [key: string]: boolean }[] = [];
-
-  //const arr1 =[] as Flash[];
-  const [arr1, setArr1] = useState([] as Flash[]);
-//   const addFlash = (flash: Flash) => {
-//   setArr1(prevArr1 => [...prevArr1, flash]);
-// };
-
-const addKeyValue = (obj: {[key: number]: boolean}, key:number, value:boolean) => {
-  obj[key] = value;
-};
-//const obj1 = {} as {[key: string]: boolean};
-const [obj1, setObj1] = useState({} as {[key: string]: boolean})
-const updateObj1 = (key: number, value: boolean) => {
-    setObj1(prevObj1 => ({
-      ...prevObj1,
-      [key]: value
-    }));
+  const [flashedTeamsIds, setFlashedTeamsIds] = useState([] as number[]);
+  const updateFlashedTeams = (id: number) => {
+    setFlashedTeamsIds((prevIds) => [...prevIds, id]);
   };
 
   searchedTeams.map((team) => {
-    const id = team.id;
     const isNewMessage = team.id === teamWithNewMessage.team_id;
-    const obj={} as { [key: string]: boolean } ;
-    obj[id] = isNewMessage;
-   //isNewMessage&& arr1.push(obj);
-   const isTeamId = !Object.keys(obj1).find(key=> +key===team.id);
-   console.log(' isTeamId ', isTeamId );
-   console.log('Object.keys(obj1)',Object.keys(obj1));
-   
-   isTeamId&&isNewMessage&& updateObj1(team.id,true);
-   console.log("obj1", obj1);
-    // !Object.keys(arrFlashIdBool).find(key=> +key===team.id)&&
-    // isNewMessage&&dispatch(setArrFlashIdBool(arr1))
+    const isTeamId = !flashedTeamsIds.find((id) => +id === team.id);
+    console.log(" isTeamId ", isTeamId);
+    isTeamId && isNewMessage && updateFlashedTeams(team.id);
+    console.log("flashedTeamsIds", flashedTeamsIds);
     return null;
   });
- 
-  //console.log("arrayWithFlashTeamId", arrayWithFlashTeamId);
-  // console.log("arr1", arr1);
-  // console.log("arrFlasId", arrFlashIdBool);
 
   return (
     <div>
