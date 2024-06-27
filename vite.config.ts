d680bee31bd6177063f7749412fb7cbdc5f4ebaa
yanakiv30 <138647535+yanakiv30@users.noreply.github.com>
2024-06-27 07:35:03 +0300

    buld

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    port: 3000, // Портът по подразбиране е 3000, но можете да го промените
   },
   build: {
-    outDir: 'dist', 
-  }
+    outDir: 'build', // Папката, където ще бъде генериран продукционният билд
+  },
 });
